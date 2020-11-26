87d825bc0049a452930aef9f8d2e765397306ccf
Kevin Luo <kvn.luo@gmail.com>
2020-11-25 23:30:56 -0500

    set up websocketlink and httplink to separate operation types

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,41 @@
 import React from "react";
 import Chat from "./components/Chat";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+
 import {
-  createHttpLink,
+  split,
+  HttpLink,
   ApolloClient,
   ApolloProvider,
   InMemoryCache,
 } from "@apollo/client";
 
-const httpLink = createHttpLink({
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:4000/",
+  options: {
+    reconnect: true,
+  },
+});
+
+const httpLink = new HttpLink({
   uri: "http://localhost:4000/",
 });
 
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
