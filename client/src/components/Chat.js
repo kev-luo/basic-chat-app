41d6b0106b6f6bd4b04872813b3c94433258f7ae
Kevin Luo <kvn.luo@gmail.com>
2020-11-25 22:02:06 -0500

    input styling button

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,18 +1,54 @@
 import React from "react";
-import { Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container } from "@material-ui/core";
 import { useQuery } from "@apollo/client";
 import { GET_MESSAGES_QUERY } from "../utils/graphql";
 
 import Message from "./Message";
 
 export default function Chat() {
+  const classes = useStyles();
   const { data } = useQuery(GET_MESSAGES_QUERY);
 
   return (
-    <Container>
+    <div className={classes.root}>
       {data?.getMessages.map((message) => {
         return <Message key={message.id} message={message} />;
       })}
-    </Container>
+      <div className={classes.input}>
+        <form>
+          <input placeholder="Type a message" type="text"/>
+          <button>
+            Send a message
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: '0.65',
+  },
+  input: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '62px',
+    borderTop: '1px solid lightgray',
+    '& form': {
+      flex: 1,
+      display: 'flex',
+    },
+    '& input': {
+      flex: 1,
+      borderRadius: '30px',
+      padding: '10px',
+      border: 'none',
+      outlineWidth: 0,
+    }
+  }
+}))
--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,22 +1,33 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
-export default function Message({message}) {
+export default function Message({ message }) {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <div className={classes.messageBody}>
-        {message.content}
+      <div className={classes.username}>
+        {message.user.slice(0, 2).toUpperCase()}
       </div>
+      <div className={classes.messageBody}>{message.content}</div>
     </div>
-  )
+  );
 }
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
-    paddingBottom: "1em"
+    paddingBottom: "1em",
+  },
+  username: {
+    height: '50px',
+    lineHeight: '50px',
+    width: '50px',
+    marginRight: "0.5rem",
+    border: "2px solid #e5e6ea",
+    borderRadius: 50,
+    textAlign: "center",
+    fontSize: "18pt",
   },
   messageBody: {
     background: "#58bf56",
@@ -24,5 +35,5 @@ const useStyles = makeStyles(theme => ({
     padding: "1em",
     borderRadius: "1em",
     maxWidth: "60%",
-  }
-}))
+  },
+}));
