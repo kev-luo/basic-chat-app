5b981577e3240395f0bd4a224b4e2206fb31e155
Kevin Luo <kvn.luo@gmail.com>
2020-11-25 22:13:29 -0500

    form creation for user message

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,14 +1,27 @@
-import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import { Container } from "@material-ui/core";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
 import { useQuery } from "@apollo/client";
 import { GET_MESSAGES_QUERY } from "../utils/graphql";
 
 import Message from "./Message";
 
 export default function Chat() {
+  const [msg, setMsg] = useState({content: '', user: ''});
   const classes = useStyles();
   const { data } = useQuery(GET_MESSAGES_QUERY);
+
+  const handleSubmit = () => {
+    console.log(msg);
+  }
+
+  const handleChange = e => {
+    const { value, name } = e.target;
+    setMsg({
+      ...msg,
+      [name]: value,
+    })
+  }
 
   return (
     <div className={classes.root}>
@@ -16,39 +29,37 @@ export default function Chat() {
         return <Message key={message.id} message={message} />;
       })}
       <div className={classes.input}>
-        <form>
-          <input placeholder="Type a message" type="text"/>
-          <button>
-            Send a message
-          </button>
+        <form onSubmit={handleSubmit} >
+          <input name="content" value={msg.Content} onChange={handleChange} placeholder="Type a message" type="text" />
+          <Button color="primary">Send a message</Button>
         </form>
       </div>
     </div>
   );
 }
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexDirection: 'column',
-    flex: '0.65',
+    display: "flex",
+    flexDirection: "column",
+    flex: "0.65",
   },
   input: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    height: '62px',
-    borderTop: '1px solid lightgray',
-    '& form': {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "62px",
+    borderTop: "1px solid lightgray",
+    "& form": {
       flex: 1,
-      display: 'flex',
+      display: "flex",
     },
-    '& input': {
+    "& input": {
       flex: 1,
-      borderRadius: '30px',
-      padding: '10px',
-      border: 'none',
+      borderRadius: "30px",
+      padding: "10px",
+      border: "none",
       outlineWidth: 0,
-    }
-  }
-}))
+    },
+  },
+}));
--- client/src/utils/graphql.js
+++ client/src/utils/graphql.js
@@ -9,3 +9,9 @@ export const GET_MESSAGES_QUERY = gql`
     }
   }
 `
+
+export const CREATE_MSG_MUTATION = gql`
+  mutation {
+    
+  }
+`
